523dedc2a89f19e2d071b615788d070e992ff71c
gogulsekar <gokulnathanc@gmail.com>
2016-07-31 23:02:26 +0530

    async controller samples

---
+++ MvcBootstrap/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using MvcBootstrap.Models;
+using MvcBootstrap.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MvcBootstrap.Controllers
+{
+    public class CalculatorController : AsyncController
+    {
+        //
+        // GET: /Calculator/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void AddAsync(CalculatorDataModel Data)
+        {
+            AsyncManager.OutstandingOperations.Increment();
+            CalculatorService svc = new CalculatorService();
+            svc.Completed += (sender, e) => {
+                AsyncManager.Parameters["output"] = (e as ServiceEventArgs).Result.ToString();
+                AsyncManager.OutstandingOperations.Decrement();
+            };
+            svc.AddAsync(Convert.ToInt32(Data.OperandLeft), Convert.ToInt32(Data.OperandRight));
+            
+        }
+
+        public string AddCompleted(string output)
+        {
+            return output;
+        }
+
+        [HttpPost]
+        public string Mul(CalculatorDataModel Data)
+        {
+            Data.Result = (Convert.ToInt32(Data.OperandLeft) * Convert.ToInt32(Data.OperandRight)).ToString();
+            return Data.Result;
+        }
+
+    }
+}
---
+++ MvcBootstrap/Service/CalculatorService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MvcBootstrap.Service
+{
+    public class CalculatorService
+    {
+        public EventHandler Completed;
+
+        public CalculatorService()
+        {
+
+        }
+
+        public async Task<int> AddAsync(int a, int b)
+        {
+            int output = await Task.Run(() => add(a, b));
+            Completed(this, new ServiceEventArgs(output));
+            return output;
+        }
+
+        private int add(int a, int b)
+        {
+            Thread.Sleep(10000);
+            return a + b;
+        }
+    }
+
+    public class ServiceEventArgs : EventArgs
+    {
+        public int Result { get; set; }
+        public ServiceEventArgs(int value)
+        {
+            Result = value;
+        }
+    }
+}
